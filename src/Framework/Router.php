8a241d45a1067c47690e8543db85a1e9139e0c79
Youngjae Lim <limyoungjae@gmail.com>
2023-12-30 23:27:09 -0500

    Add regex for advanced routing - part I

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -27,10 +27,12 @@ class Router
      */
     public function match(string $path): array|bool
     {
-        foreach ($this->routes as $route) {
-            if ($route['path'] === $path) {
-                return $route['params'];
-            }
+        $pattern = '#^/(?<controller>[a-z]+)/(?<action>[a-z]+)$#';
+
+        if (preg_match($pattern, $path, $matches)) {
+            $matches = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+
+            return $matches;
         }
 
         return false;

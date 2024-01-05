f8bbab0bdf41259af2d15977871382a6da61f562
Youngjae Lim <limyoungjae@gmail.com>
2024-01-05 11:48:25 -0500

    Move the routes to a separate config file

---
+++ config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+$router = new Framework\Router;
+
+// Add the most specific routes first
+$router->add('/admin/{controller}/{action}', ['namespace' => 'Admin']);
+$router->add('/{title}/{id:\d+}/{page:\d+}', ['controller' => 'products', 'action' => 'showPage']);
+$router->add('/product/{slug:[\w-]+}', ['controller' => 'products', 'action' => 'show']);
+$router->add('/{controller}/{id:\d+}/{action}');
+$router->add('/home/index', ['controller' => 'home', 'action' => 'index']);
+$router->add('/products', ['controller' => 'products', 'action' => 'index']);
+$router->add('/', ['controller' => 'home', 'action' => 'index']);
+$router->add('/{controller}/{action}');
+
+return $router;
--- public/index.php
+++ public/index.php
@@ -18,17 +18,7 @@ if ($path === false) {
     throw new UnexpectedValueException("Malformed URL: '{$_SERVER['REQUEST_URI']}'");
 }
 
-$router = new Framework\Router;
-
-// Add the most specific routes first
-$router->add('/admin/{controller}/{action}', ['namespace' => 'Admin']);
-$router->add('/{title}/{id:\d+}/{page:\d+}', ['controller' => 'products', 'action' => 'showPage']);
-$router->add('/product/{slug:[\w-]+}', ['controller' => 'products', 'action' => 'show']);
-$router->add('/{controller}/{id:\d+}/{action}');
-$router->add('/home/index', ['controller' => 'home', 'action' => 'index']);
-$router->add('/products', ['controller' => 'products', 'action' => 'index']);
-$router->add('/', ['controller' => 'home', 'action' => 'index']);
-$router->add('/{controller}/{action}');
+$router = require '../config/routes.php';
 
 $container = new Framework\Container;
 

15dd4a106d0210fac09177e2c872a70292141e59
Youngjae Lim <limyoungjae@gmail.com>
2024-01-08 08:41:16 -0500

    Create a base template and modify the index template to extend it

--- views/Products/index.mvc.php
+++ views/Products/index.mvc.php
@@ -1,25 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <!-- <link rel="stylesheet" href="/css/style.css"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
-<title>{{ title }}</title>
-</head>
-<body>
-<h1>Products</h1>
+{% extends "base.mvc.php" %}
 
-<a href="/products/new">New Product</a>
+{% block title %}Products{% endblock %}
 
-<p>Total: {{ total }} </p>
+{% block body %}
 
-{% foreach ($products as $product): %}
-    <h2>
-        <a href="/products/{{ product['id'] }}/show">
-            {{ product['name'] }}
-        </a>
-    </h2>
-{% endforeach %}
+    <h1>Products</h1>
 
-</body>
-</html>
+    <a href="/products/new">New Product</a>
+
+    <p>Total: {{ total }} </p>
+
+    {% foreach ($products as $product): %}
+        <h2>
+            <a href="/products/{{ product['id'] }}/show">
+                {{ product['name'] }}
+            </a>
+        </h2>
+    {% endforeach %}
+
+{% endblock %}
+
---
+++ views/base.mvc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <link rel="stylesheet" href="/css/style.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
+<title>{% yield title %}</title>
+</head>
+<body>
+
+{% yield body %}
+
+</body>
+</html>
